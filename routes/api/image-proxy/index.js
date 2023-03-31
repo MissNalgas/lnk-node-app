8b75a917ebd149ceb3fd4b9c785bafad7995a6b2
Juan Aragon <me@juanaragon.co>
2023-03-31 13:11:08 -0500

    (feat): Add jpeg option to image-proxy

--- routes/api/image-proxy/index.js
+++ routes/api/image-proxy/index.js
@@ -5,13 +5,17 @@ router.get('/', async (req, res) => {
 
 	try {
 		const {default: got} = await import('got');
-		const { img, width, height } = req.query;
+		const { img, width, height, jpeg = false } = req.query;
 
 		if (!img) return res.status(400).send({message: 'Invalid url'});
 		const wNumber = Number(width) || 600;
 		const hNumber = Number(height) || undefined;
 
-		const sharpStream = sharp().resize({width: wNumber, height: hNumber});
+		let sharpStream = !jpeg ? (
+			sharp().resize({width: wNumber, height: hNumber})
+		) : (
+			sharp().resize({width: wNumber, height: hNumber}).flatten({background: '#ffffff'}).jpeg()
+		);
 		
 		got.stream(img).pipe(sharpStream).pipe(res);
 	} catch(err) {
@@ -20,6 +24,22 @@ router.get('/', async (req, res) => {
 		res.status(500).send({message: 'Server error'});
 	}
 
+});
+
+router.get('/url', async (req, res) => {
+
+	try {
+		const {default: got} = await import('got');
+		const {url} = req.query;
+		if (!url) return res.status(400).send('bad request');
+
+		got.stream(url).pipe(res);
+
+	} catch(err) {
+		console.error(err);
+		res.status(500).send({err});
+	}
+	
 });
 
 module.exports = router;
