7e2243195eed02f17a9a23812ea8cd7f328a0c93
Juan Aragon <me@juanaragon.co>
2023-04-04 17:39:02 -0500

    (feat): Cache images after being transformed

--- routes/api/image-proxy/index.js
+++ routes/api/image-proxy/index.js
@@ -1,28 +1,45 @@
 const router = require('express').Router();
 const sharp = require('sharp');
+const NodeCache = require('node-cache');
 
-const storage = new Map();
+
+const cache = new NodeCache({stdTTL: 600, checkPeriod: 60});
 
 router.get('/', async (req, res) => {
 
 	try {
-		const {default: got} = await import('got');
-		const { img, width, height, jpeg = false } = req.query;
 
-		if (!img) return res.status(400).send({message: 'Invalid url'});
-		const wNumber = Number(width) || 600;
-		const hNumber = Number(height) || undefined;
-
-		let sharpStream = !jpeg ? (
-			sharp().resize({width: wNumber, height: hNumber})
-		) : (
-			sharp().resize({width: wNumber, height: hNumber}).flatten({background: '#ffffff'}).jpeg()
-		);
+		let { img, width = 600, height, jpeg = false, bg = 'transparent' } = req.query;
+		if (!img) {
+			return res.status(400).send('Img not found');
+		}
+		const background = bg;
+		console.log({background});
+		width = Number(width);
+		height = Number(height) || undefined;
+		if (isNaN(width)) return res.status(400).send('Invalid width');
+		const configKey = JSON.stringify({img, width, height, jpeg, background});
+		const value = cache.get(configKey);
+		res.set('Content-Type', jpeg ? 'image/jpeg' : 'image/png');
+		if (value) {
+			return res.send(value);
+		} 
 		
-		got.stream(img, {cache: storage}).pipe(sharpStream).pipe(res);
+		const {default: got} = await import('got');
+		const data = await got(img).buffer();
+		let processedData;
+		if (jpeg) {
+			processedData = await sharp(data).resize({width, height, fit: 'contain', background}).flatten({background}).jpeg().toBuffer();
+		} else {
+			processedData = await sharp(data).resize({width, height, fit: 'contain', background}).png().toBuffer();
+		}
+		cache.set(configKey, processedData);
+
+		return res.send(processedData);
+
 	} catch(err) {
 		console.error(err);
-
+		res.set('Content-Type', 'application/json');
 		res.status(500).send({message: 'Server error'});
 	}
 
